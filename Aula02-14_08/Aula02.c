6c22838dc81c547ade72762de4323a75095d1173
Matheus Leandro <matleandrosilva@gmail.com>
2024-08-14 21:45:01 -0300

    update p2 aula, nao sei pq funcionou
    copiei o codigo do kudlake e funcionou na minha maquina

--- Aula02-14_08/Aula02.c
+++ Aula02-14_08/Aula02.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 
 int main()
 {
-    int completo = 90;
-    float floate = 90.5;
-    char caractere = 's';
-
-    printf("%d\n%f\n%c\n", completo,floate, caractere);
-
+    float numeros [2];
+    for(int i = 0; i < 3; i++)
+    {
+        int pos = 0;
+        printf("Digite um numero: \n");
+        scanf("%f", &numeros[pos]);
+    }
+    float media = (numeros[0] + numeros[1] + numeros[2]) /3;
+    printf("Sua media eh: %f", media);
     return 0;
 }
