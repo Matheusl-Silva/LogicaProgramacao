ce8bc9dcc240f5be5730b0263e746db627592385
Matheus Leandro <matleandrosilva@gmail.com>
2024-08-16 21:10:48 -0300

    exercicio incremento

--- aula03-16_08/aula03.c
+++ aula03-16_08/aula03.c
@@ -3,19 +3,9 @@
 
 int main()
 {
-    float num1,num2,num3,resultado;
-    
+  int x;
+  printf("Digite um valor: ");
+  scanf("%d", &x);
+  printf("O valor foi adicionado com +1: %d", x + 1);
 
-    printf("Digite um numero para uma surpresa: ");
-    scanf("%d", &num1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &num2);
-    printf("Digite o terceiro numero: ");
-    scanf("%d", &num3);
-
-    resultado = 2 * ((float)(num1 - num3) / 8) - num2 * 5;
-
-    printf("O resultado da equacaoo x = 2 * (( a - c)/8) - b * 5, eh: %f", resultado);
-
-    return 0;
 }  
