c4ec7f80a9dfd6f159fee1e2119329e261474484
Matheus Leandro <matleandrosilva@gmail.com>
2024-08-28 19:57:44 -0300

    update: aula05
    bhaskaraaaaaaa

--- Aula05-28_08/aula05.c
+++ Aula05-28_08/aula05.c
@@ -1,24 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int idade;
-    
-    printf("Digite sua idade: ");
-    scanf("%d", &idade);
+    double a, b, c, delta, x1, x2;
 
-    if (idade < 18)
+    printf("Digite tres valores para resolver bhaskara: \n");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    delta = pow(b,2) - 4 * a * c;
+
+    if(delta < 0)
     {
-        printf("Voce eh menor de idade");
-    }else if (idade > 18 && idade < 65)
-    {
-        printf("Voce eh maior de idade");
+        printf("A equacao esta errada bobao");
     }
     else
     {
-        printf("Parabens voce eh um velhote");
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        printf("x1 = %lf\n", x1);
     }
-    
+        if (delta > 0)
+        {
+            x2 = (-b - sqrt(delta)) / (2 * a);
+            printf("x2 = %lf\n", x2);
+        }
+        
+
     
 }
