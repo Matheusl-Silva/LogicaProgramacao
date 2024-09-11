6f6714969357b8eab06c15e91ef783d312fbbcc8
Matheus Leandro <matleandrosilva@gmail.com>
2024-09-11 20:19:55 -0300

    update: Aula07
    marretada

---
+++ Aula07-11_09/aula07.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        printf("Teste %d", i);
+        i++;
+    } while (i <= 10);
+
+    return 0;
+    
+
+}
