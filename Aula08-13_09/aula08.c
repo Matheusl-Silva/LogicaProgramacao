b928a7591a21bf3fbea79268d309b88393debf0e
Matheus Leandro <matleandrosilva@gmail.com>
2024-09-13 20:46:37 -0300

    add: Exercicio de Fibonnaci
    Resolução do exercício de fibonacci em C

---
+++ Aula08-13_09/aula08.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int input, t1 = 0, t2 = 2, next;
+
+   printf("Digite um numero:\n");
+   scanf("%d", &input);
+
+   for (int i = 1; i <= input; i=t1)
+   {
+        printf("%d \n", t1);
+
+        next = t1 + t2;
+        t1 = t2;
+
+        t2 = next;
+   }
+   
+}
