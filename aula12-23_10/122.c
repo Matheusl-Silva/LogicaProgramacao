7f6d9d53e252da18810c582804e30c174283d2a0
Matheus Leandro <matleandrosilva@gmail.com>
2024-10-23 21:42:02 -0300

    update: Exercicio

--- aula12-23_10/122.c
+++ aula12-23_10/122.c
@@ -8,13 +8,13 @@ int main()
 
     for(int l = 0; l < 5;  l++){
         for(int c = 0; c < 3; c++){
-            printf("Semestre do aluno: %d\ninsira a nota do aluno: %d\n", l+1, c+1);
+            printf("Semestre do aluno: %d\ninsira a nota %d do aluno:\n", l+1, c+1);
             scanf("%d", &facul[l][c]);
         }
     }
         for(int l = 0; l < 5;  l++){
         for(int c = 0; c < 3; c++){
-            printf("No semestre: %d o aluno tirou a nota: %d\n", c+1, facul[l][c]);
+            printf("No semestre: %d o aluno %d tirou a nota: %d\n", c+1, l+1, facul[l][c]);
         }
     }
     
