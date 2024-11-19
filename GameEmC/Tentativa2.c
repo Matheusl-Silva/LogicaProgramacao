c76884fd3864c2f0a68a259afbb46485f48c2acc
Matheus Leandro <matleandrosilva@gmail.com>
2024-11-19 12:52:50 -0300

    create tentativa 2

---
+++ GameEmC/Tentativa2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct{
+    nome[70];
+    int tentativas;
+} jogardor;
+
+void instruicoes()
+{
+    printf("\n-- Instrucoes do jogo --\n");
+    printf("")
+}
